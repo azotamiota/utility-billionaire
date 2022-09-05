1307abcdbc97ccd12d89b11681483aae2a0f8c6c
agiskorska <skorskaga@gmail.com>
2022-09-05 14:57:39 +0100

    Add Join component layout

--- src/pages/JoinGame/index.jsx
+++ src/pages/JoinGame/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Button, Container, Input, Title } from '../../components';
 import styles from './index.module.css'
 
 function JoinGame() {
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log('this should connect me to the right room')
+  }
   return (
-    <div><h1>Welcome to Join Game page!!</h1></div>
+    <div>
+      <Title>Welcome to Join Game page!!</Title>
+      <Container>
+        <Input type='text' classVariant='light'>Enter Room Name</Input>
+        <Input type='text' classVariant='light'>Choose your username</Input>
+        <Button classVariant='dark' text='Join Room' handleClick={handleClick}></Button>
+      </Container>
+    </div>
   )
 }
 
