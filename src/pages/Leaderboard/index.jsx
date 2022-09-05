7318e204bdf014bdb9c2aa31e9fdea022ce5c77d
azotamiota <azotamiota@gmail.com>
2022-09-05 15:53:15 +0100

    update Leaderboard component

--- src/pages/Leaderboard/index.jsx
+++ src/pages/Leaderboard/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import styles from './index.module.css'
 
 function Leaderboard() {
-  return (
-    <div><h1>Welcome to Leaderboard page!</h1></div>
+
+  const [leaderboard, setLeaderboard] = useState(null)
+
+  const fetchUsers = async () => {
+    const fetchedUsers = await axios.get('https://utility-billionaire.herokuapp.com/leaderboard')
+    const sortedUsers = fetchedUsers.data.users.sort((a, b) => b.score - a.score)
+    setLeaderboard(sortedUsers)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  return (<>
+    <div><h1>Leaderboard:</h1></div>
+    {console.log('leaderboard: ', leaderboard)}
+    {leaderboard && leaderboard.slice(0, 11).map(user => {
+      return <>
+      <h5 key={user._id}>{user.username}</h5>
+      <div key={user._id}>{user.score}</div>
+      </>
+    })}
+  </>
   )
 }
 
-export default Leaderboard
+export default Leaderboard;
