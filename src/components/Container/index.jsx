5670399f85165115a69337896e43920d7553f7f0
agiskorska <skorskaga@gmail.com>
2022-09-05 11:29:22 +0100

    Create Container component

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './index.module.css'
+
+function Container({children}) {
+  return (
+    <div className={styles.root}>{children}</div>
+  )
+}
+
+export default Container
