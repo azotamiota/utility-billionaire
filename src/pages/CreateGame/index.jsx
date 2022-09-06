087691ec7a463f4e210d582a1c64a78433bc5e5f
agiskorska <skorskaga@gmail.com>
2022-09-06 09:55:51 +0100

    Add Context

--- src/pages/CreateGame/index.jsx
+++ src/pages/CreateGame/index.jsx
@@ -1,23 +1,24 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 import styles from './index.module.css'
 import { Button, Container, Footer, Input, Header } from '../../components'
+import { useQuestions } from '../../context'
 
 function CreateGame() {
-  // const [username, setUsername] = useState(null);
-  // const [category, setCategory] = useState('animals');
-  // const [difficulty, setDifficulty] = useState('easy');
-  // const [timeOut, setTimeOut] = useState(false);
-  // const [questionNumber, setQuestionNumber] = useState(1);
-  // const [earned, setEarned] = useState("$ 0");
-  // const [data, setData] = useState([])
-
-
+  const [username, setUsername] = useState(null);
+  const [category, setCategory] = useState(17);
+  const [difficulty, setDifficulty] = useState('easy');
+  const [start, setStart] = useState(false)
+  const { data, setData } = useQuestions()
+  const navigateTo = useNavigate()
   const topics = [
     {id: 1, name: 'Any Category', value:0},
     {id: 2, name: 'Science', value:17},
     {id: 3, name: 'General Knowledge', value:9},
   ]
-  const difficulty = [
+  const levels = [
     {id: 1, name: 'easy', value: 'easy'},
     {id: 2, name: 'medium', value: 'medium'},
     {id: 3, name: 'hard', value: 'hard'},
@@ -25,10 +26,12 @@ function CreateGame() {
 
   const startGame = (e) => {
     e.preventDefault()
-    // async function searchApi() {
-    // const result = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`);
-    // setData(result.data);
-    // }
+    async function searchApi() {
+      const result = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`);
+      setData(result.data.results)
+      navigateTo('/game')
+    }
+    searchApi()
   }
 
   return (
@@ -36,7 +39,7 @@ function CreateGame() {
       <h1>Create your utility billionaire game!</h1>
       <Container>
         <Input type='select' classVariant='light' name='category' defaultValue={topics}>Category</Input>
-        <Input type='select' classVariant='light' name='difficulty' defaultValue={difficulty}>Difficulty</Input>
+        <Input type='select' classVariant='light' name='difficulty' defaultValue={levels}>Difficulty</Input>
         <Input type='text' classVariant='light' name='room' defaultValue=''>Room Name</Input>
         <Input type='text' classVariant='light' name='room' defaultValue=''>Username</Input>
         <Button text='Start' handleClick={startGame} classVariant='neonText'/>
