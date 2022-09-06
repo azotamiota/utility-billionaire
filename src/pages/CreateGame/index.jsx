b2ba01d8af040530c125ec2bf2ca9c3d73e9f308
agiskorska <skorskaga@gmail.com>
2022-09-06 16:59:00 +0100

    Add sockets

--- src/pages/CreateGame/index.jsx
+++ src/pages/CreateGame/index.jsx
@@ -1,16 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 import styles from './index.module.css'
 import { Button, Container, Footer, Input, Header, Title } from '../../components'
-import { useQuestions } from '../../context'
-
+import { SocketContext } from '../../context'
 function CreateGame() {
+
   const [category, setCategory] = useState(17);
   const [difficulty, setDifficulty] = useState('easy');
-  const { data, setData } = useQuestions()
+  const [room, setRoom] = useState('')  
+  const [username, setUsername] = useState('')
+  // const { data, setData } = useQuestions()
   const navigateTo = useNavigate()
+  const socket = useContext(SocketContext);
   const topics = [
     { id: 9, value: 9, name: "General Knowledge"},
     { id: 10, value: 10, name: "Entertainment: Books"},
@@ -29,15 +32,19 @@ function CreateGame() {
     {id: 3, name: 'hard', value: 'hard'},
   ]
 
+
   const startGame = (e) => {
     e.preventDefault()
     async function searchApi() {
       console.log(difficulty, category)
       const result = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`);
-      setData(result.data.results)
-      navigateTo('/game')
+      // setData(result.data.results)
+      console.log(username, room)
+      socket.emit('send_question', {room, questions: result.data.results})
+      socket.emit('join_room',  {username, room})
     }
     searchApi()
+    navigateTo('/waiting')
   }
 
   return (
@@ -46,8 +53,8 @@ function CreateGame() {
       <Container>
         <Input type='select' classVariant='light' name='category' defaultValue={topics} onChange={(e) => setCategory(e.target.value)}>Category</Input>
         <Input type='select' classVariant='light' name='difficulty' defaultValue={levels} onChange={(e) => setDifficulty(e.target.value)}>Difficulty</Input>
-        <Input type='text' classVariant='light' name='room' defaultValue=''>Room Name</Input>
-        <Input type='text' classVariant='light' name='room' defaultValue=''>Username</Input>
+        <Input type='text' classVariant='light' name='room' defaultValue={room} onChange={(e) => setRoom(e.target.value)}>Room Name</Input>
+        <Input type='text' classVariant='light' name='user' defaultValue={username}  onChange={(e) => setUsername(e.target.value)}>Username</Input>
         <Button text='Start' handleClick={startGame} classVariant='neonText'/>
       </Container>
     </div>
