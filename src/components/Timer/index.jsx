253ddf17af66c6af2d10a89a24cbfe97868db5e1
azotamiota <azotamiota@gmail.com>
2022-09-09 12:11:50 +0100

    change Timer for presentation

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -5,7 +5,7 @@ import styles from './index.module.css'
 const Timer = ({ timeOut, questionNumber }) => {
 
   
-  const [timer, setTimer] = useState(5);
+  const [timer, setTimer] = useState(8);
 
   useEffect(() => {
     if (timer === 0) return timeOut() 
@@ -17,7 +17,7 @@ const Timer = ({ timeOut, questionNumber }) => {
 
   useEffect(() => {
 
-    setTimer(5);
+    setTimer(8);
 
   }, [questionNumber]); 
   
