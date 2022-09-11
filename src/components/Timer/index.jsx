8f331e3b6ae3c4f44aa7018ca70ebd59e6a87b3c
azotamiota <azotamiota@gmail.com>
2022-09-11 17:50:26 +0100

    set Timer to 30 seconds

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -5,7 +5,7 @@ import styles from './index.module.css'
 const Timer = ({ timeOut, questionNumber }) => {
 
   
-  const [timer, setTimer] = useState(7);
+  const [timer, setTimer] = useState(30);
 
   useEffect(() => {
     if (timer === 0) return timeOut() 
@@ -17,7 +17,7 @@ const Timer = ({ timeOut, questionNumber }) => {
 
   useEffect(() => {
 
-    setTimer(7);
+    setTimer(30);
 
   }, [questionNumber]); 
   
